7f42a021290562e8797ed688f4d80fed9e0dbf28
Zmagis <eglegustaite@gmail.com>
2020-07-01 12:44:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Flickr = require('flickr-sdk');
+const { response } = require('express');
+require('dotenv').config();
+
+const app = express();
+const API_KEY = process.env.FLICKR_API_KEY;
+
+let flickr = new Flickr(API_KEY);
+
+app.get('/data', (req, res) => {
+  console.log('REQUEST!!!!!!!!!!!!!!!!!!!!', Object.keys(req));
+  const page = req.query['0'];
+  console.log('PAGE', page);
+  flickr.photos
+    .search({
+      tags: 'forest',
+      per_page: 10,
+      page: page,
+    })
+    .then((r) => {
+      res.json(r);
+    })
+    .catch(function (err) {
+      console.error('bonk', err);
+    });
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
